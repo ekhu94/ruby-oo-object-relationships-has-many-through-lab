7b3647c459ba36877e4ea1bd8d2bae4aa2d6b52a
Erik Huang <erikhuangdrums@gmail.com>
2021-01-15 17:11:15 -0800

    commit tree d2de5e0cdc958fad0e481395c8da5d0aded7f649

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,18 @@
+class Appointment
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    attr_accessor :date, :patient, :doctor
+
+    def initialize(date, patient, doctor)
+        @date, @patient, @doctor = date, patient, doctor
+        save
+    end
+
+    def save
+        @@all << self
+    end
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,30 @@
+class Doctor
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select { |appt| appt.doctor == self }
+    end
+
+    def patients
+        appointments.map { |appt| appt.patient }
+    end
+end
