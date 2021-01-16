dfbcfafeefcff83051699f1be81f1c5289c96431
Erik Huang <erikhuangdrums@gmail.com>
2021-01-15 17:11:32 -0800

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,31 @@
+class Artist
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select { |song| song.artist == self }
+    end
+
+    def genres
+        songs.map { |song| song.genre }
+    end
+
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
+class Genre
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def songs
+        Song.all.select { |song| song.genre == self }
+    end
+
+    def artists
+        songs.map { |song| song.artist }
+    end
+end
